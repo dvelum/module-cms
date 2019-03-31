cf708794d54294a82970715dd2b95efdadc88769
k-samuel <kirill.a.egorov@gmail.com>
2019-03-31 21:28:48 +0300

    Routing

---
+++ configs/frontend.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    /*
+   * the type of frontend router with two possible values:
+   * 'Module' — using tree-like page structure  (‘Pages’ section of the back-office panel);
+   * 'Path' — the router based on the file structure of client controllers.
+   * 'Config' - using frontend modules configuration
+   */
+    'router' => 'Cms', // 'Cms','Path','Config'
+    // Default Frontend Controller
+    'default_controller' => '\\Dvelum\\App\\Frontend\\Cms\\Index\\Controller',
+];
